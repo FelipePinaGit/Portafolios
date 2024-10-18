d4bfef032b4f96caecf8551eb8a63be5b434036e
Felipe Piña <felipepina39@gmail.com>
2024-10-18 19:51:49 -0300

    Add files via upload

---
+++ Profolio version 3/static/script.js
@@ -0,0 +1,35 @@
+function openNav() {
+    document.getElementById("sideNav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideNav").style.width = "0";
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    closeNav();
+});
+
+const skillIcons = document.querySelectorAll('.skill img');
+
+skillIcons.forEach(icon => {
+    icon.addEventListener('mouseover', () => {
+        icon.style.transform = 'scale(1.2)';
+        icon.style.transition = 'transform 0.3s';
+    });
+    icon.addEventListener('mouseout', () => {
+        icon.style.transform = 'scale(1)';
+    });
+});
